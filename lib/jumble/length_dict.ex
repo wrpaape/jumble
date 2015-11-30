cd1b267eca87bc048a67491f3107774117910f45
Reid Paape <wrpaape@gmail.com>
2015-11-29 20:52:44 -0600

    2fast

--- lib/jumble/length_dict.ex
+++ lib/jumble/length_dict.ex
@@ -1,11 +1,12 @@
 defmodule Jumble.LengthDict do
-  @dict File.read!("/usr/share/dict/words")
+  # @dict File.read!("/usr/share/dict/words")
 
   alias Jumble.Helper
-  
-  def get(length_word) do
+
+  def get(length_word, string_id) do
     __MODULE__
     |> Agent.get(Map, :get, [length_word])
+    |> apply(:get, [string_id])
   end
 
   def start_link(%{final_lengths: final_lengths, jumble_maps: jumble_maps} = args) do
@@ -25,15 +26,31 @@ defmodule Jumble.LengthDict do
 
   def build_dict(lengths) do
     lengths
-    |> Enum.map_join("|",fn(length) ->
-      length
-      |> Integer.to_string
-      |> Helper.cap("\\w{", "}")
+    |> Enum.map(fn(length) ->
+      module_string =
+        "Length"
+        <> Integer.to_string(length)
+
+      name_spaced_module =
+        __MODULE__
+        |> Module.safe_concat(module_string)
+
+      {length, name_spaced_module}
     end)
-    |> Helper.cap("\\b(", ")\\b")
-    |> Regex.compile!
-    |> Regex.scan(@dict, capture: :all_but_first)
-    |> List.flatten
-    |> Enum.group_by(&byte_size/1)
+    |> Enum.into(Map.new)
   end
+
+  # def build_dict(lengths) do
+  #   lengths
+  #   |> Enum.map_join("|",fn(length) ->
+  #     length
+  #     |> Integer.to_string
+  #     |> Helper.cap("\\w{", "}")
+  #   end)
+  #   |> Helper.cap("\\b(", ")\\b")
+  #   |> Regex.compile!
+  #   |> Regex.scan(@dict, capture: :all_but_first)
+  #   |> List.flatten
+  #   |> Enum.group_by(&byte_size/1)
+  # end
 end
