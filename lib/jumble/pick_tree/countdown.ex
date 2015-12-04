43001e9cf260dd7815dc6c936c72953b8ffa3b9e
Reid Paape <wrpaape@gmail.com>
2015-12-03 22:26:25 -0600

    lots of ctr z

--- lib/jumble/pick_tree/countdown.ex
+++ lib/jumble/pick_tree/countdown.ex
@@ -1,28 +1,24 @@
 defmodule Jumble.PickTree.Countdown do
-  alias Jumble.Helper
   alias Jumble.PickTree
   alias Jumble.Solver
 
-  def start_link(caller_pid, timeout) do
-    Agent.start_link(fn -> {timeout, caller_pid} end, name: __MODULE__)
-  end
+  def start_link(timeout), do: Agent.start_link(fn -> timeout end, name: __MODULE__)
 
   def start_countdown,     do: Agent.cast(__MODULE__, &start_countdown/1)
 
   def reset_countdown,     do: Agent.cast(__MODULE__, &reset_countdown/1)
 
 
-  def start_countdown({timeout, caller_pid}) do
+  def start_countdown(timeout) do
     __MODULE__
-    |> spawn(:countdown, [timeout, caller_pid])
-    |> Helper.wrap_prepend(caller_pid)
+    |> spawn(:countdown, timeout)
   end
 
-  def reset_countdown(state = {_caller_pid, countdown_pid}) do
+  def reset_countdown(countdown_pid) do
     countdown_pid
     |> send(:reset)
 
-    state
+    countdown_pid
   end
 
   def countdown(timeout) do
