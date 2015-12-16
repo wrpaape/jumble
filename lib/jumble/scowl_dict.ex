6fc3b5bd1d20509fd96715ecf3db15772c9bf62a
Reid Paape <wrpaape@gmail.com>
2015-12-16 01:26:45 -0600

    YUS

--- lib/jumble/scowl_dict.ex
+++ lib/jumble/scowl_dict.ex
@@ -7,26 +7,27 @@ defmodule Jumble.ScowlDict do
 # ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓#
 
   def safe_get(length_word, string_id) do
-    [length_word, :all_modules]
-    |> get_in_agent
-    |> Enum.find_value(&get_in_dict(&1, string_id))
+    length_word
+    |> get_in_sizes_map(:all_modules)
+    |> Enum.find_value(&apply_dict(&1, :get, string_id))
   end
 
   def limited_get(length_word, string_id) do
-    [length_word, :limited_module]
-    |> get_in_agent
-    |> get_in_dict(string_id)
+    length_word
+    |> get_in_sizes_map(:limit_module)
+    |> apply_dict(:get, string_id)
   end
 
   def safe_valid_id?(length_word, string_id) do
-    [length_word, :safe_valid_ids]
-    # |> get_in_agent
-    |> valid_id?(string_id)
+    length_word
+    |> get_in_sizes_map(:last_module)
+    |> apply_dict(:valid_id?, string_id)
   end
 
   def limited_valid_id?(length_word, string_id) do
-    [length_word, :limited_valid_ids]
-    |> valid_id?(string_id)
+    length_word
+    |> get_in_sizes_map(:limit_module)
+    |> apply_dict(:valid_id?, string_id)
   end
 
   def update_limit do
@@ -51,21 +52,20 @@ defmodule Jumble.ScowlDict do
   def update_limit(scowl_dict) do
     scowl_dict
     |> Enum.reduce(Map.new, fn({length, sizes_map}, next_scowl_dict)->
-      {[{next_limited_module, next_valid_ids}], trans_sizes_map} =
-        sizes_map
-        |> Map.get_and_update(:rem_limits, &Enum.split(&1, 1))
 
-      next_sizes_map =
-        trans_sizes_map
-        |> Map.put(:limited_module, next_limited_module)
-        |> Map.put(:limited_valid_ids, next_valid_ids)
+      {[next_limit_module], trans_sizes_map} =
+        sizes_map
+        |> Map.get_and_update(:rem_modules, &Enum.split(&1, 1))
+
+        next_sizes_map =
+          trans_sizes_map
+          |> Map.put(:limit_module, next_limit_module)
 
       next_scowl_dict
       |> Map.put(length, next_sizes_map)
     end)
   end
 
-  def reset_limit
 
   def build_dicts(lengths) do
     lengths
@@ -74,32 +74,17 @@ defmodule Jumble.ScowlDict do
         "Length"
         <> Integer.to_string(length)
 
-        {all_modules, [{limited_module, valid_ids} | rem_limits]} =
-          @dict_sizes
-          |> List.foldr({[], []}, fn(dict_size, {modules, limits})->
-            module =
-              dict_size
-              |> build_module(length_module)
-
-            valid_ids =
-              module
-              |> apply(:valid_ids, [])
-
-            {[module | modules], [{module, valid_ids} | limits]}
-          end)
-
-      safe_valid_ids =
-        rem_limits
-        |> List.last
-        |> elem(1)
+        all_modules =
+          [limit_module | rem_modules] =
+            @dict_sizes
+            |> Enum.map(&build_module(&1, length_module))
 
       sizes_map =
         Map.new
         |> Map.put(:all_modules, all_modules)
-        |> Map.put(:limited_module, limited_module)
-        |> Map.put(:limited_valid_ids, valid_ids)
-        |> Map.put(:rem_limits, rem_limits)
-        |> Map.put(:safe_valid_ids, safe_valid_ids)
+        |> Map.put(:limit_module, limit_module)
+        |> Map.put(:rem_modules, rem_modules)
+        |> Map.put(:last_module, List.last(rem_modules))
 
       scowl_dict
       |> Map.put(length, sizes_map)
@@ -110,21 +95,15 @@ defmodule Jumble.ScowlDict do
 ####################################### helpers ########################################
 # ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓#
   
-  defp valid_id?(valid_id_key_path, string_id) do
-    valid_id_key_path
-    |> get_in_agent
-    |> Set.member?(string_id)
-  end
-
   defp build_module(dict_size, length_module) do
     [__MODULE__, "Size" <> dict_size, length_module]
     |> Module.safe_concat
   end
 
-  defp get_in_agent(key_path), do: Agent.get(__MODULE__, Kernel, :get_in, [key_path])
+  defp get_in_sizes_map(length, key), do: Agent.get(__MODULE__, Kernel, :get_in, [[length, key]])
 
-  defp get_in_dict(dict_size_module, string_id) do
-    dict_size_module
-    |> apply(:get, [string_id])
+  defp apply_dict(dict_module, fun, string_id) do
+    dict_module
+    |> apply(fun, [string_id])
   end
 end
