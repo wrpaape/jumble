a90df6917f6f02618cf7b20d33142a3258e7f8b5
Reid Paape <wrpaape@gmail.com>
2015-12-20 10:31:32 -0600

    deleting lots of comments, lengthdict

--- lib/jumble/scowl_dict/builder/server_builder.ex
+++ lib/jumble/scowl_dict/builder/server_builder.ex
@@ -21,33 +21,3 @@ defmodule Jumble.ScowlDict.Builder.ServerBuilder do
     end
   end
 end
-
-# defmodule Jumble.ScowlDict.Builder.ServerBuilder do
-#   defmacro build_server(size, length) do
-#     module =
-#       [Jumble, ScowlDict, "Size#{size}", "Length#{length}"]
-#       |> Module.concat
-
-#     quote do
-#       defmodule unquote(module) do
-#         @dict __MODULE__
-#           |> Module.concat(Dict)
-#           |> apply(:get, [])
-
-#         @valid_ids @dict
-#           |> Map.keys
-#           |> Enum.into(HashSet.new)
-
-#         def get(string_id) do
-#           @dict
-#           |> Map.get(string_id)
-#         end
-
-#         def valid_id?(string_id) do
-#           @valid_ids
-#           |> Set.member?(string_id)
-#         end
-#       end
-#     end
-#   end
-# end
